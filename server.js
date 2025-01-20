58a8eb4ed10995e56299c30d390d431e8cb9d3d3
kacerato <79872194+kacerato@users.noreply.github.com>
2025-01-20 20:39:12 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+{
+  "name": "shootout-score",
+  "version": "1.0.0",
+  "description": "Kings League statistics tracker",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1",
+    "cors": "^2.8.5",
+    "axios": "^0.27.2",
+    "cheerio": "^1.0.0-rc.12"
+  }
+  }
